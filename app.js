25bd17dddd4909ffa8f83985ab8fc37ce6b31f96
Robert <dorlan25@hotmail.com>
2019-09-11 09:01:50 +0000

    change port to process.env.PORT

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('*', function(req, res) {
 
 
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT, function(req, res){
 	console.log("Porfolio app listening on port 3000");
 });
